0586196182258346806a72e521acb78cb2e8550c
amycham <amycham@gmail.com>
2019-03-12 10:58:18 -0700

    added availabile.php

---
+++ pricing-tool/available.php
@@ -0,0 +1,292 @@
+
+
+<?php
+	$here = "available";
+	$floor = "rooms";
+	$title = "Available Rooms";
+	$head = "<style type='text/css'>
+			@import url(roomfeatures.css);
+		</style>";
+	$body = "<table cellspacing='0' class='roomfeatures'>
+	<thead>
+		<tr>
+		<th>Available</th>
+		<th>Address</th>
+		<th>Room</th>
+		<th>Bath</th>
+		<th>Furnished</th>
+		<th>Rent</th>
+	
+		
+			
+			
+			
+			
+			
+			
+			<tr><td><B><H3>Available over summer</H3></b></td></tr>	
+			
+			
+						
+			
+			</tr>
+			<tr><td>8.1.17-8.14.17</td>
+			<td class='featurename'><a href='181crit3rd.php'>181 Crittenden Blvd</a></li></td>
+			<td>Penthouse south</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>680</td>
+			
+			
+			
+			
+			
+			</tr>
+			
+			
+			
+			
+			
+		
+			
+			
+			
+			
+			
+			
+			
+			<
+			
+			<tr><td>7.1.17-7.31.17</td>
+			<td class='featurename'><a href='168irv1st.php'>168 Irvington Rd</a></li></td>
+			<td>Master North</td>			
+			<td>private</td>
+			<td>no</td>
+			<td>675</td>
+			
+			<tr><td>8.2.17-8.25.17</td>
+			<td class='featurename'><a href='226cas2nd.php'>226 Castleman Rd</a></li></td>
+			<td>West</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>685</td>
+			
+			</tr>
+			
+			
+			<tr><td>6.15.17-7.31.18</td>
+			<td class='featurename'><a href='141ros2nd.php'>141 rossiter Rd</a></li></td>
+			<td>Study</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>635</td>
+			
+			</tr>
+			
+			
+			<tr><td><B><H3>Available August 2017</H3></b></td></tr>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			</tr>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			<tr><td>8.1.17 P</td>
+			<td class='featurename'><a href='95crit2nd.php'>95 Crittenden Blvd</a></li></td>
+			<td>Corner Room</td>			
+			<td>private</td>
+			<td>no</td>
+			<td>605</td>
+			
+			</tr>
+			
+			
+			
+			
+			
+			
+			<tr><td>8.15.17</td>
+			<td class='featurename'><a href='197ral1st.php'>197 Raleigh Str</a></li></td>
+			<td>North</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>630</td>
+			
+			</tr>
+			
+			
+			
+			</tr>
+			
+			
+			
+			
+			
+			
+			
+			
+			<tr>
+			
+			
+			
+			
+			
+			
+			<tr><td><B><H3>Available September 2017</H3></b></td></tr>
+			
+			</tr>
+			
+			
+			
+			
+			<tr><td>9.1.17 P</td>
+			<td class='featurename'><a href='94ral2nd.php'>94 Raleigh Str</a></li></td>
+			<td>Master North</td>			
+			<td>shared</td>
+			<td>yes</td>
+			<td>655</td>
+			
+			
+			
+			<tr><td>9.1.17 P </td>
+			<td class='featurename'><a href='89crit1st.php'>89 Crittenden Blvd</a></li></td>
+			<td>Front Bedroom</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>625</td>
+			
+			
+			</tr>
+			
+			<tr><td>9.1.17 P</td>
+			<td class='featurename'><a href='44irv2nd.php'>44 Irvington Rd</a></li></td>
+			<td>Master West</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>670</td>
+			
+			
+			</tr>
+			
+			
+			
+			
+			
+			
+			
+			<tr><td><B><H3>Available Janaury 2018</H3></b></td></tr>
+			
+			</tr>
+			
+			
+			
+			<tr><td>1.1.18 P</td>
+			<td class='featurename'><a href='44irv2nd.php'>44 Irvington Rd</a></li></td>
+			<td>Master West</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>670</td>
+			
+			
+			</tr>
+			
+			
+			<tr><td>1.1.18</td>
+			<td class='featurename'><a href='65irv2nd.php'>65 Irvington Rd</a></li></td>
+			<td>2nd fl Master North East</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>670</td>
+			
+			<tr><td>1.1.18</td>
+			<td class='featurename'><a href='65irv2nd.php'>65 Irvington Rd</a></li></td>
+			<td>2nd fl Master North West</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>655</td>
+			
+			
+			<tr><td>1.1.18 P</td>
+			<td class='featurename'><a href='65irv2nd.php'>65 Irvington Rd</a></li></td>
+			<td>2nd fl Master South East</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>655</td>
+			
+			<tr><td>1.1.18</td>
+			<td class='featurename'><a href='205irv2nd.php'>205 Irvington Rd</a></li></td>
+			<td>2nd fl Master South West</td>			
+			<td>private</td>
+			<td>no</td>
+			<td>660</td>
+			
+			
+			
+			<tr><td>1.1.18</td>
+			<td class='featurename'><a href='226cas2nd.php'>226 Castleman Rd</a></li></td>
+			<td>2nd fl Master South</td>			
+			<td>private</td>
+			<td>yes</td>
+			<td>670</td>
+			
+			
+			<tr><td>P=Pending contract</td>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			<tr><td><B><H3><P>
+			
+			
+			
+			
+			</tr>
+			
+			
+			
+			
+				
+			
+		
+			
+			
+			
+			
+	
+		
+		
+			 
+			
+			
+		
+		
+		
+		
+	
+		
+		
+	</tbody>
+</table>";
+	
+			include("roomfeatures.php");			
+			include("main.php");
+?>
