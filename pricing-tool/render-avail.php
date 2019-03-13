fb7fb307fad078fc482cc4b78136db68ea532b58
amycham <amycham@gmail.com>
2019-03-13 15:25:52 -0700

    date string conversion and format, and replacement in array working

--- pricing-tool/render-avail.php
+++ pricing-tool/render-avail.php
@@ -40,21 +40,12 @@
             break;
         }
 
-
-
-        //temp basic table rendering
-        /*$table_rendering.="<tr><td>".$csv[$i]['availDate'].$csv[$i]['availStatus']."</td>".
-          "<td>".$csv[$i]['streetAddress']."</td>".
-          "<td>".$csv[$i]['roomName']."</td>".
-          "<td>".$csv[$i]['privateBath']."</td>".
-          "<td>".$csv[$i]['furnished']."</td>".
-          "<td>".$csv[$i]['rent']."</td></tr>";*/
-
-
-        //sort into arrays on availability status
-
         $i++;
     }
+    $avail_confirmed = sort_array($avail_confirmed, 'availDate');
+    //$avail_possible = sort_array($avail_possible, 'availDate');
+    //$avail_pending = sort_array($avail_pending, 'availDate');
+
     //render tables by availStatus
     $table_rendering.=make_table('avail-confirmed', $avail_confirmed, "Confirmed Availabilities");
     $table_rendering.=make_table('avail-pending', $avail_pending, "Contracts Pending");
@@ -104,5 +95,39 @@
 
   return $table_body;
 }
+/***********************************************
+** Sort multidimensional array on specified field value
+** $data: array to sort
+** $criterai: field to sort on
+************************************************/
+function sort_array($data, $criteria){
+    $rows = $data;
+    $i = 0;
+
+    foreach($rows as $row) {
+    //convert availDate strings to date
+      if($criteria == 'availDate'){
+        //get availDate
+        $current_date = $data[$i]['availDate'];
+
+        //convert availDate string to date
+        $conv_date = strtotime($current_date);
+
+        //format date
+        $fmt_date = date('m.d.Y', $conv_date);
+
+        //replace availDate
+        $data[$i]['availDate'] = $fmt_date;
+
+        $i++;
+      }
+    }
+      //sort array
+
+      //return sorted array
+      print_r($data);
+      return $data;
+
+  }
 
 ?>
